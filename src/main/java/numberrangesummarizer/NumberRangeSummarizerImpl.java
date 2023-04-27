fc26c7fb57bb34e528f1959a1d68f9420d68416d
ShakurV <shakurvariawa01@gmail.com>
2023-04-27 14:29:05 +0200

    Created Unit Tests

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -12,36 +12,40 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
         //convert the strings into integers
         for (String s : stringArray) {
-            numberList.add(Integer.parseInt(s));
+            try {
+                numberList.add(Integer.parseInt(s));
+            }catch (Exception e){
+                System.out.println("Failed to collect.\nCould not convert \""+s+"\" into an Integer");
+            }
         }
 
         return numberList;
     }
 
-    //Assumes numbers are always in ascending order
+    //Assumes number ranges are always in ascending order
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        int i = 0;
+        int nextValue = 0;                      //denotes the next value we are looking for if it is sequential
         boolean range = false;
         StringBuilder output = new StringBuilder();
         Iterator<Integer> it = input.iterator();
 
-        if (it.hasNext()) {
-            i = it.next();
-            output.append(i);
-            ++i;
+        if (it.hasNext()) {                     //gets the first value in the collection
+            nextValue = it.next();
+            output.append(nextValue);
+            ++nextValue;
         }
 
         while (it.hasNext()) {
             int tmp = it.next();
 
-            if (i == tmp) {
-                range = true;           //open a new range
-                ++i;
+            if (nextValue == tmp) {
+                range = true;                   //open a new range
+                ++nextValue;
             } else {
-                if (range) {            //closes up a range once we look at a number that is not also sequential
+                if (range) {                    //closes up a range once we look at a number that is not also sequential
                     output.append('-');
-                    output.append(--i); //decrement to get last in order number
+                    output.append(--nextValue); //decrement to get last in order number
                     output.append(", ");
                     output.append(tmp);
                     range = false;
@@ -49,13 +53,13 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
                     output.append(", ");
                     output.append(tmp);
                 }
-                i = tmp + 1;        //init i to look for next in order number
+                nextValue = tmp + 1;            //init nextValue to look for next in order number
             }
         }
 
-        if (range) {            //closes up a range in case last numbers were sequential
+        if (range) {                            //closes up a range in case last numbers were sequential
             output.append('-');
-            output.append(--i); //decrement to get last in order number
+            output.append(--nextValue);         //decrement to get last in order number
         }
 
         return output.toString();
---
+++ src/test/java/numberrangesummarizertest/NumberRangeSummarizerImplTest.java
@@ -0,0 +1,117 @@
+package numberrangesummarizertest;
+
+import numberrangesummarizer.NumberRangeSummarizer;
+import numberrangesummarizer.NumberRangeSummarizerImpl;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+public class NumberRangeSummarizerImplTest {
+    private NumberRangeSummarizer numSum;
+
+    @Before
+    public void before(){
+        numSum = new NumberRangeSummarizerImpl();
+    }
+
+    public void verify(String input, String expectedOutput){
+        Collection<Integer> numCollection = numSum.collect(input);
+        String out = numSum.summarizeCollection(numCollection);
+        assertEquals(expectedOutput,out);
+    }
+
+    @Test
+    public void testEmptyString(){
+        verify("","");
+    }
+
+    @Test
+    public void testSuppliedInput(){
+        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+        String expectedOutput = "1, 3, 6-8, 12-15, 21-24, 31";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testEndOnRange(){
+        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24";
+        String expectedOutput = "1, 3, 6-8, 12-15, 21-24";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testStartOnRange(){
+        String input = "6,7,8,12,13,14,15,21,22,23,24,31";
+        String expectedOutput = "6-8, 12-15, 21-24, 31";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testDuplicates(){
+        String input = "6,7,7,8,12,13,14,15,21,22,23,24,31";
+        String expectedOutput = "6-7, 7-8, 12-15, 21-24, 31";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testOneNumber(){
+        String input = "6";
+        String expectedOutput = "6";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testOneRange(){
+        String input = "6,7,8";
+        String expectedOutput = "6-8";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testDuplicateRanges(){
+        String input = "6,7,8,6,7,8";
+        String expectedOutput = "6-8, 6-8";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testBadInput(){
+        String input = "6,7,8,6,K,8";
+        String expectedOutput = "6-8, 6, 8";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testNegatives(){
+        String input = "-5,-4,-3,-1,0,2,5";
+        String expectedOutput = "-5--3, -1-0, 2, 5";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testDecimals(){
+        String input = "0,0.5,1,3";
+        String expectedOutput = "0-1, 3";
+
+        verify(input,expectedOutput);
+    }
+
+    @Test
+    public void testBadFormat(){
+        String input = ",6,7,,8,12,13,,14,15,21,22,23,,24,31";
+        String expectedOutput = "6-8, 12-15, 21-24, 31";
+
+        verify(input,expectedOutput);
+    }
+}
