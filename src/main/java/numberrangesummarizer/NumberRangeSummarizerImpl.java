8f640d9bbe852b0d240db3492a057d57ecc19b94
ShakurV <shakurvariawa01@gmail.com>
2023-04-27 12:09:46 +0200

    collection method

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -1,11 +1,24 @@
 package numberrangesummarizer;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Arrays;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
     @Override
     public Collection<Integer> collect(String input) {
-        return null;
+        List<Integer> numberList = new ArrayList<>();
+
+        //split the input into an array of strings
+        String[] stringArray = input.split(",");
+
+        //convert the strings into integers
+        for (String s : stringArray) {
+            numberList.add(Integer.parseInt(s));
+        }
+
+        return numberList;
     }
 
     @Override
