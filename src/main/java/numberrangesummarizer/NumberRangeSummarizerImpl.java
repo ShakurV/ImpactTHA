d8eb6519706b89b8bd678007a8bd10f03f91484c
ShakurV <shakurvariawa01@gmail.com>
2023-04-27 11:36:44 +0200

    setup implementation boilerplate

---
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -0,0 +1,15 @@
+package numberrangesummarizer;
+
+import java.util.Collection;
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
+    @Override
+    public Collection<Integer> collect(String input) {
+        return null;
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return null;
+    }
+}
